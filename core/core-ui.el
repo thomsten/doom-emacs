498e28d94aafabd3c7d84f758342de0d897d0621
Thomas Stenersen <stenersen.thomas@gmail.com>
2019-03-05 10:57:57 +0100

    Don't display line numbers

--- core/core-ui.el
+++ core/core-ui.el
@@ -359,7 +359,7 @@ read-only or not file-visiting."
 ;;; Line numbers
 
 ;; line numbers in most modes
-(add-hook! (prog-mode text-mode conf-mode) #'display-line-numbers-mode)
+;(add-hook! (prog-mode text-mode conf-mode) #'display-line-numbers-mode)
 
 (defun doom|enable-line-numbers ()  (display-line-numbers-mode +1))
 (defun doom|disable-line-numbers () (display-line-numbers-mode -1))
