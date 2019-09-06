cca8ab57893c14461257333de268dfd6fd3eda8a
Stenersen, Thomas <thomas.stenersen@nordicsemi.no>
2019-09-06 12:55:33 +0200

    Fix org-mode after update

--- modules/lang/org/config.el
+++ modules/lang/org/config.el
@@ -15,6 +15,7 @@
     ;; org-irc
     ;; org-mhe
     ;; org-rmail
+    org-tempo
     ))
 
 ;; Sub-modules
@@ -30,6 +31,10 @@
    org-faces org-entities org-list org-pcomplete org-src
    org-footnote org-macro ob org org-agenda org-capture))
 
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
 
 ;;
 ;; Packages
